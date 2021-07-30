aaa29d2a507513280cf93a97aba58c3c6dbaaf14
namihagi <43953318+namihagi@users.noreply.github.com>
2021-07-31 01:26:26 +0900

    js-app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,66 @@
-const name = "tanaka";
-const greet = `My name is ${name}`;
-console.log(greet);
+import "./styles.css";
 
-const arr1 = [1, 3];
-console.log(arr1);
-console.log(...arr1);
+/* <li>
+  <div class="list-row">
+    <p>ToDo 1</p>
+    <button>Completed</button>
+    <button>Delete</button>
+  </div>
+</li> */
 
-const nameArr = ["tanaka", "mori", "suzuki"];
-nameArr.map((name) => console.log(`${name}さん`));
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 1;
-});
-console.log(newNumArr);
+  // make row
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "Completed";
+  completeButton.addEventListener("click", () => {
+    // delete from incomplete list
+    deleteFromIncompleteList(completeButton.closest("li"));
+
+    const addTarget = completeButton.parentNode;
+    const completeText = addTarget.firstElementChild.innerText;
+    addTarget.textContent = null;
+
+    const p = document.createElement("p");
+    p.innerText = completeText;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "Back";
+
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", () => {
+    deleteFromIncompleteList(deleteButton.closest("li"));
+  });
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  const li = document.createElement("li");
+  li.appendChild(div);
+
+  // add row to list
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
