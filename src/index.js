ec7feccdc3324499da2acb634705af5d0334ce6d
namihagi <43953318+namihagi@users.noreply.github.com>
2021-07-30 15:19:13 +0900

    added some code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import "./styles.css";
+const name = "tanaka";
+const greet = `My name is ${name}`;
+console.log(greet);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const arr1 = [1, 3];
+console.log(arr1);
+console.log(...arr1);
+
+const nameArr = ["tanaka", "mori", "suzuki"];
+nameArr.map((name) => console.log(`${name}さん`));
+
+const numArr = [1, 2, 3, 4, 5];
+const newNumArr = numArr.filter((num) => {
+  return num % 2 === 1;
+});
+console.log(newNumArr);
